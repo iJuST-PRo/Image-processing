4534b133c108f70a06435ba4c84bec6f15101d0e
iJuST-PRo <lotfe119997@gmail.com>
2023-01-09 20:45:52 -0500

    Finished

--- image_processing.py
+++ image_processing.py
@@ -1,9 +1,6 @@
 import sys, os
 
 
-def flip_list(my_list):
-    pass
-
 def to_clean_list(file):
     as_list = []
     for line in file:
@@ -15,6 +12,32 @@ def longest_line(cln_lst):
     longest = max(len(long) for long in cln_lst)
     return longest
 
+def add_spaces(clean_list, longest):
+    equal_list = []
+    for c,x in enumerate(clean_list):
+        while len(x) < longest:
+            x += " "
+        equal_list.append(x)
+    return equal_list    
+
+def horizontal_to_vertical(longest, equal_length):
+    result = []
+    for x in range(longest):
+        for y, z in enumerate(equal_length):
+            result.append(z[x])
+            if not y % longest:
+                if not x and not y:
+                    continue
+                else:
+                    result.append('\n')
+    return result
+
+def rotate(file):
+    clean_list = to_clean_list(file.readlines())
+    longest = longest_line(clean_list)
+    equal_length = add_spaces(clean_list,longest)           
+    result = horizontal_to_vertical(longest, equal_length)
+    return result
 
 
 if __name__=='__main__':
@@ -23,29 +46,10 @@ if __name__=='__main__':
     except:
         print(f'Error: file not specified\nUsage: python {os.path.basename(__file__)} file.txt')
         exit()
-
-    with open(sys.argv[1]) as file:
-        clean_list = to_clean_list(file.readlines())
-        #print(list(file))
-
-
-        longest = longest_line(clean_list)
-
-        result = []
-        for c,x in enumerate(clean_list):
-            while len(x) < longest:
-                x += " "
-                clean_list[c] = x
-                #print(gg[c])
-        #print(gg)
-        for x in range(longest):
-            for y, z in enumerate(clean_list):
-                print(z[x], sep='', end='')
-                if y % longest == 0:
-                    print('')
-        print(result,end="\r")
-    
-
-    
-
-
+    with open("output.txt", "w") as output:
+        with open(sys.argv[1]) as file:
+            result = rotate(file)
+        
+        output.writelines(result)
+        print('Done, Check output.txt')
+        
